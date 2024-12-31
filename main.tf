0580298707ef531ca86becebc1fe13e192f76677
dewansh26 <dewanshmishra01@gmail.com>
2024-12-31 17:45:24 +0530

    terraform5

--- main.tf
+++ main.tf
@@ -34,8 +34,9 @@ resource "aws_security_group" "allow_http_ssh_https" {
   # Allow all outbound traffic
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    #to_port     = 0
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
